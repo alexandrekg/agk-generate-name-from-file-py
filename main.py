ef461dd0c3dcd4d4ba9a02ace50b6fcb8ad1c2c4
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-11-15 14:58:03 -0300

    Finalizando a refatoração

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import random
 
+def clean_name(_name):
+    return _name.replace("\n", "")
+
+
+def get_names(names_file):
+    _names = []
+    for _name in names_file:
+        _name = clean_name(_name)
+        _names.append(_name)
+
+    return _names
 
 
 def main():
-    with open('names.txt') as nomes_file:
-        lista_nomes = [nome.replace("\n", "") for nome in nomes_file]
-    print(f"Nome gerado automaicamente: {random.choice(lista_nomes)}")
+    with open('names.txt') as names_file:
+        names_list = get_names(names_file)
+
+    random_name = random.choice(names_list)
+    print(f"Nome gerado automaicamente: {random_name}")
 
 
 if __name__ == "__main__":
