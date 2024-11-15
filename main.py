f46aae2f6305392b3e52a45499b86db6f8711127
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-11-15 14:54:04 -0300

    Finalizando o projeto

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import random
+
 
 
 def main():
-    print('Hi')
+    with open('names.txt') as nomes_file:
+        lista_nomes = [nome.replace("\n", "") for nome in nomes_file]
+    print(f"Nome gerado automaicamente: {random.choice(lista_nomes)}")
 
 
 if __name__ == "__main__":
